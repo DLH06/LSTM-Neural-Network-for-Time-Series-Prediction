91981834152ddb89866300ea2156e7f34b7eb4ef
Dao Le Huy <huydao@tdt.asia>
2022-10-24 16:59:31 +0700

    convert model

--- core/model_torch.py
+++ core/model_torch.py
@@ -1,27 +1,34 @@
 import torch
 import torch.nn as nn
 
-from core.utils import Timer
-
 
 class Net(nn.Module):
-    def __init__(self, dropout_rate=0.2, input_dim=2):
+    def __init__(self, dropout_prob=0.2, input_dim=2):
         super().__init__()
-        self.first_lstm = nn.LSTM(input_dim, 100, batch_first=True, dropout=dropout_rate)
+        self.dropout = nn.Dropout(dropout_prob)
+
+        self.first_lstm = nn.LSTM(input_dim, 100, batch_first=True)
         self.second_lstm = nn.LSTM(100, 100)
-        self.third_lstm = nn.LSTM(100, 100, dropout=dropout_rate)
+        self.third_lstm = nn.LSTM(100, 100)
         self.linear = nn.Linear(100, 1)
 
     def forward(self, x):
         o_1, (h_s_1, c_s_1) = self.first_lstm(x)
+        o_1 = self.dropout(o_1)
         o_2, (h_s_2, c_s_2) = self.second_lstm(o_1)
-        o_3, (h_s_3, c_s_3) = self.second_lstm(o_2)
-        # output = self.linear(h_s_3)
-        return o_3, (h_s_3, c_s_3)
+        o_3, (h_s_3, c_s_3) = self.third_lstm(o_2)
+        output = self.linear(h_s_3)
+
+        # return o_3, (h_s_3, c_s_3)
+        return output
 
 
 if __name__ == '__main__':
     model = Net()
     input_sample = torch.rand(3, 50, 2)
-    output, (hidden_state, cell_state) = model(input_sample)
-    print(hidden_state.shape)
+    output = model(input_sample)
+    print(output.shape)
+    # output = output.squeeze(0)
+    output = output.view(-1, 50)
+    print(output.shape)
+    
